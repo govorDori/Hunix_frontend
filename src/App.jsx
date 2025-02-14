4c5fae6a7a1011e921c55e54ffb58bafeea3bef3
TheRedOwl <v.adam3000@gmail.com>
2025-02-14 11:38:18 +0100

    routing fix, animation testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-import { Header } from './components/Header'
 import { Home } from './pages/Home'
 
 const router=createBrowserRouter([
-  {element:<Header/>,
-    children:[
-      {path:"/",element:<Home/>},
-    ]
-  }
+      {path:'/',element:<Home />}
 ])
 
 function App() {
-  return <RouterProvider/>
+  return <RouterProvider router={router}/>
 }
 
 export default App
