b492162aaa76a11cbc52f02452a653a51dce7905
govorDori <dorigovor2006@gmail.com>
2025-05-28 23:47:19 +0200

    Footer finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,10 +23,10 @@ export const Footer = () => {
                 <div className=' sm:w-max w-full flex flex-col justify-center items-center pr-5 pl-5 p-2 shadow-[#e0e0e0] shadow-md rounded-lg bg-white'>
                     <h1 className='font-bold text-xl tracking-wide'>Govorkovics Dóra</h1>
                     <div className='text-center'>
-                        <p className='break-words  w-[250px]'>Szöveg magadról Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima natus labordolopernatur? Quas quaerat tenetur laudantium!</p>
-                        <p className='text-[#939393]'>Email:</p>
-                        <p className='text-[#939393]'>Tel.:</p>
-                        <p className='text-[#939393]'>Munkakör: asdasd</p>
+                        <p className='break-words  w-[250px]'>Kedvelem a webfejlesztést és világát. Számomra a legkedvesebb rész a design megtervezése és megvalósítása.</p>
+                        <p className='text-[#939393]'>Email: dorigovor2006@gmail.com</p>
+                        <p className='text-[#939393]'>Tel.: +36 70 514 8645</p>
+                        <p className='text-[#939393]'>Munkakör: Programozó</p>
                     </div>
                 </div>
 
@@ -35,7 +35,7 @@ export const Footer = () => {
                     <div className='text-center'>
                         <p className='break-words  w-[250px]'>Kezdő programozó vagyok, főként weboldalak front és backend részéhez értek.</p>
                         <p className='text-[#939393]'>Email: v.adam3000@gmail.com</p>
-                        <p className='text-[#939393]'>Tel.: 06302873725</p>
+                        <p className='text-[#939393]'>Tel.: +36 30 287 3725</p>
                         <p className='text-[#939393]'>Munkakör: Programozó</p>
                     </div>
                 </div>
@@ -43,10 +43,10 @@ export const Footer = () => {
                 <div className='sm:w-max w-full flex flex-col justify-center items-center pr-5 pl-5 p-2 shadow-[#e0e0e0] shadow-md rounded-lg bg-white'>
                     <h1 className='font-bold text-xl tracking-wide'>Molnár Nándor</h1>
                     <div className='text-center'>
-                        <p className='break-words  w-[250px]'>Szöveg magadról Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima natus labordolopernatur? Quas quaerat tenetur laudantium!</p>
-                        <p className='text-[#939393]'>Email:</p>
-                        <p className='text-[#939393]'>Tel.:</p>
-                        <p className='text-[#939393]'>Munkakör: asdasd</p>
+                        <p className='break-words  w-[250px]'>Front -és backend programozó vagyok, már több ügyfél weboldaláért vagyok felelős.</p>
+                        <p className='text-[#939393]'>Email: ggamerek123@gmail.com</p>
+                        <p className='text-[#939393]'>Tel.: +36 30 432 5533</p>
+                        <p className='text-[#939393]'>Munkakör: Programozó</p>
                     </div>
                 </div>
 
